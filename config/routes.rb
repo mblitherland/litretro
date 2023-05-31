6fd0acb939c5ab53ae4fed17ffc1a52b029ef915
Michael Litherland <michael.litherland@gmail.com>
2023-05-31 16:13:17 -0400

    Route to a default active retro page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :retros
+  get '/active', to: 'active#index'
 
   # Defines the root path route ("/")
   # root "articles#index"
