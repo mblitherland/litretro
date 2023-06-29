b92050c241a7bee962e90ca1dcb00a257ba5f36c
Michael Litherland <michael.litherland@gmail.com>
2023-06-29 16:13:52 +0000

    Made the active retro page do useful things

--- app/controllers/active_controller.rb
+++ app/controllers/active_controller.rb
@@ -2,6 +2,19 @@ class ActiveController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    render
+    @retros = Retro
+      .where(user_id: current_user.id)
+      .where.not(state: [ "setup", "complete" ])
+    @retros += Retro
+      .where.not(user_id: current_user.id)
+      .where.not(state: [ "setup", "complete" ])
+      .joins(:participants)
+      .where(participants: { user_id: current_user.id })
+
+    if @retros.length == 1
+      redirect_to "/#{@retros[0].state}/#{@retros[0].id}"
+    else
+      render
+    end
   end
 end
