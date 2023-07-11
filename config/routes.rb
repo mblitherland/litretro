9076cfd46dae019ea558d7aa06c0191634e0c582
Michael Litherland <michael.litherland@gmail.com>
2023-07-11 20:16:08 +0000

    Newly registered users should verify themselves now

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # get 'home/index'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :retros do
