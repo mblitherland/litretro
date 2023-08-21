0913a650bc162fb185adc575de3a58606c4ba01b
Michael Litherland <michael.litherland@gmail.com>
2023-08-21 15:30:27 +0000

    Partial work done for creating guest user

--- app/controllers/guest_controller.rb
+++ app/controllers/guest_controller.rb
@@ -27,6 +27,24 @@ class GuestController < ApplicationController
 
   def join_guest
     return if disabled?
+
+    @retro = Retro.where(guest_link: params[:id]).first
+
+    if !@retro.nil? && !current_user.nil?
+
+      # TODO: Working here
+
+      @retro.participants.create({
+        retro_id: params[@retro.id],
+        email: current_user.email,
+        user_id: current_user.id,
+        guest: true
+      })
+      render 'joined'
+    else
+      redirect_to '/', status: 403
+    end
+
   end
 
   def join_registered
