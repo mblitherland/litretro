821fae48a2ad341e007a23726e3a4dc6a58efd63
Michael Litherland <michael.litherland@gmail.com>
2024-02-04 01:09:05 +0000

    Model relationships updated for schema migration

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,5 +2,8 @@ class Card < ApplicationRecord
   belongs_to :column
   belongs_to :user
 
+  belongs_to :parent, class_name: 'Card'
+  has_many :cards, foreign_key: 'parent_card'
+
   has_many :comments
 end
